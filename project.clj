3894e6dc51046f0e808c966bd8eb39aba6739838
tommy <thmorriss@gmail.com>
2022-06-11 18:22:07 -0400

    init commit

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject sorter-pipeline "0.1.0-SNAPSHOT"
+            :description "FIXME: write description"
+            :url "http://example.com/FIXME"
+            :dependencies [[lambdacd "0.14.7"]
+                           [lambdaui "1.1.0" :exclusions [lambdacd]]
+                           [http-kit "2.3.0"]
+                           [org.clojure/clojure "1.9.0"]
+                           [org.clojure/tools.logging "0.3.1"]
+                           [org.slf4j/slf4j-api "1.7.5"]
+                           [ch.qos.logback/logback-core "1.0.13"]
+                           [ch.qos.logback/logback-classic "1.0.13"]]
+            :profiles {:uberjar {:aot :all}}
+            :main sorter-pipeline.core)
---
+++ src/sorter_pipeline/core.clj
@@ -0,0 +1,31 @@
+(ns sorter-pipeline.core
+  (:require
+    [sorter-pipeline.pipeline :as pipeline]
+    [sorter-pipeline.ui-selection :as ui-selection]
+    [org.httpkit.server :as http-kit]
+    [lambdacd.runners :as runners]
+    [lambdacd.core :as lambdacd]
+    [clojure.tools.logging :as log])
+  (:import (java.nio.file.attribute FileAttribute)
+    (java.nio.file Files LinkOption))
+  (:gen-class))
+
+(defn- create-temp-dir []
+  (str (Files/createTempDirectory "lambdacd" (into-array FileAttribute []))))
+
+(defn -main [& args]
+  (let [;; the home dir is where LambdaCD saves all data.
+        ;; point this to a particular directory to keep builds around after restarting
+        home-dir (create-temp-dir)
+        config   {:home-dir home-dir
+                  :name     "sorter pipeline"}
+        ;; initialize and wire everything together
+        pipeline (lambdacd/assemble-pipeline pipeline/pipeline-def config)
+        ;; create a Ring handler for the UI
+        app      (ui-selection/ui-routes pipeline)]
+    (log/info "LambdaCD Home Directory is " home-dir)
+    ;; this starts the pipeline and runs one build after the other.
+    ;; there are other runners and you can define your own as well.
+    (runners/start-one-run-after-another pipeline)
+    ;; start the webserver to serve the UI
+    (http-kit/run-server app {:port 8080})))
